3e0a6797e28f85cb7d3e5513261e786396b8d86a
E. Dunham <github@edunham.net>
2018-10-12 20:26:55 -0700

    cussedness take 2

--- hamper/plugins/whome.py
+++ hamper/plugins/whome.py
@@ -6,13 +6,16 @@ from hamper.utils import ude
 
 from whometxt import *
 
+
 def art(noun):
-        if noun[0] in ['a', 'e', 'i', 'o', 'u', 'y']:
-            return "an " + noun
-        return "a " + noun
+    if noun[0] in ['a', 'e', 'i', 'o', 'u', 'y']:
+        return "an " + noun
+    return "a " + noun
+
 
 def punc():
-    return random.choice(['.','...','!','?'])
+    return random.choice(['.', '...', '!', '?'])
+
 
 class WhoMePlugin(ChatPlugin):
     """ What someone said, and how it harmed you
@@ -29,28 +32,39 @@ class WhoMePlugin(ChatPlugin):
         for p in parts:
             if p.lower() in whome:
                 founds.append(p)
-        if len(founds)>0:
+        if len(founds) > 0:
             return random.choice(founds)
         return False
 
+    def composed(self):
+        # noun-verb noun-noun, thanks https://creativeswears.livejournal.com/
+        ers = [v.replace("ing", "er") for v in verbs if v.endswith("ing")]
+        out = '-'.join(random.choice(nouns), random.choice(verbs))
+        out += " "
+        out = '-'.join(random.choice(nouns), random.choice(nouns + ers))
+        return out
+
     def repa(self, prefix):
-        out = prefix + "? "                         # vile?
-        out += random.choice(mopes)                 # gosh
-        out += ", I must be "                       # , I must be
-        out += art(random.choice(bigs))             # a big
-        out += " " + random.choice(whome)           # failure
+        out = prefix + "? "  # vile?
+        out += random.choice(mopes)  # gosh
+        out += ", I must be "  # , I must be
+        out += art(random.choice(bigs))  # a big
+        out += " " + random.choice(whome)  # failure
         return out
 
     def repb(self, prefix):
-        out =  prefix                               # barbaric
-        out += "? Only " + art(random.choice(bigs)) # ? Only a massive
+        out = prefix  # barbaric
+        out += "? Only " + art(random.choice(bigs))  # ? Only a massive
         out += " " + random.choice(whome) + "ing "  # failureing
-        out += random.choice(whome) + " "           # terrible
-        out += random.choice(whome)                 # toaster
-        out += " would call some"                   # would call someone
+        if random.random() < .5:
+            out += self.composed()
+        else:
+            out += random.choice(whome) + " "  # terrible
+            out += random.choice(whome)  # toaster
+        out += " would call some"  # would call someone
         out += random.choice(['one ', 'thing ', 'body '])
-        out += art(prefix)                          # a barbaric
-        out += punc()                               # !
+        out += art(prefix)  # a barbaric
+        out += punc()  # !
         return out
 
     def repc(self, prefix):
@@ -59,8 +73,28 @@ class WhoMePlugin(ChatPlugin):
     def repd(self, prefix):
         return "Who me, " + art(prefix) + "?"
 
+    def repe(self, prefix):
+        out = "you " + self.composed()
+        # "you hat-wearing food-eater"
+        if random.random() < .5:
+            out = "Why " + out
+            # why you hat-wearing food-eater
+        if random.random() < .5:
+            if random.random() < .5:
+                out += " you " + random.choice("oughtta", "go and", "go",
+                                               "better")
+                # why you hat-wearing food-eater you better
+            out += " " + random.choice(verbs)
+            # why you hat-wearing food-eater stinking OR
+            # why you hat-wearing food-eater you better stinking
+            out += " " + art(self.composed)
+        out += punc()
+
     def whomereply(self, bot, comm, prefix):
-        resp = random.choice([self.repa, self.repb, self.repc, self.repd])(prefix)
+        resp = random.choice([
+            self.repa, self.repb, self.repc, self.repd, self.repe, self.repe,
+            self.repe
+        ])(prefix)
         bot.reply(comm, '{0}: {1}'.format(comm['user'], resp))
 
     def manners(self, bot, comm, prefix):
@@ -73,7 +107,7 @@ class WhoMePlugin(ChatPlugin):
                 self.whomereply(bot, comm, prefix)
                 return True
         elif comm['directed']:
-            out = "Now now, " + random.choice(['saying','calling me'])
+            out = "Now now, " + random.choice(['saying', 'calling me'])
             out += " " + prefix
             out += " isn't very nice."
             bot.reply(comm, '{0}: {1}'.format(comm['user'], out))
@@ -82,12 +116,9 @@ class WhoMePlugin(ChatPlugin):
             # Politely ignore unkind language in on-topic channels
             return False
 
-
     def message(self, bot, comm):
         msg = ude(comm['message'].strip())
         prefix = self.discusses_me(msg)
         if prefix:
             return self.manners(bot, comm, prefix)
         return False
-
-
